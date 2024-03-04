9961a3dba8044fe3a1c6645e6cef00b5b85f7e2a
kat-wang <kate.a.wang@utexas.edu>
2024-03-03 22:54:56 -0600

    added tutorial

--- scenes/entities/air_state.gd
+++ scenes/entities/air_state.gd
@@ -4,7 +4,7 @@ class_name AirState
 
 @export var landing_state : State
 @export var attack_state : State
-@export var double_jump_velocity : float = -600.0
+@export var double_jump_velocity : float = -800.0
 @export var landing_sfx : AudioStreamPlayer2D
 
 var has_doubled_jumped = false
@@ -13,6 +13,7 @@ func state_process(delta):
 	if character.is_on_floor():
 		landing_sfx.play()
 		next_state = landing_state
+	
 
 func state_input(event : InputEvent):
 	if event.is_action_pressed("up") && !has_doubled_jumped:
