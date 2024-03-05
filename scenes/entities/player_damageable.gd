ad0f060de9bea13e913fb6329328ad57eb75e22f
kat-wang <kate.a.wang@utexas.edu>
2024-03-04 23:07:08 -0600

    adding health bar/hud system

---
+++ scenes/entities/player_damageable.gd
@@ -0,0 +1,13 @@
+extends Damageable
+
+
+func hit(damage : int, knockback_direction : Vector2):
+	print("hit")
+	health -= damage
+	emit_signal("on_hit", get_parent(), damage, knockback_direction)
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == dead_animation_name):
+		get_parent().queue_free()
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
