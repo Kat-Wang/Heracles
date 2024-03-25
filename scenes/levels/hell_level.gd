0ba5c50337d3b4ffd4889d6b54852dcff246c660
kat-wang <kate.a.wang@utexas.edu>
2024-03-25 13:21:05 -0500

    hell level added (wip)

---
+++ scenes/levels/hell_level.gd
@@ -0,0 +1,20 @@
+extends Level
+
+signal level_complete(next_level:PackedScene)
+
+@onready var bgm := $AudioStreamPlayer2D
+@onready var hell_portal := $HellPortal
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bgm.play()
+	hell_portal.portal_entered.connect(transition)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func transition():
+	level_complete.emit(next_level)
