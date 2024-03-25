81dc47f4f7579f95e1b3955c3cf645fd2e81999e
kat-wang <kate.a.wang@utexas.edu>
2024-03-25 11:22:22 -0500

    fixed skip button in opening cutscene

--- scenes/lore.gd
+++ scenes/lore.gd
@@ -2,13 +2,18 @@ extends Node
 
 @onready var anim_player := $AnimationPlayer
 @onready var bgm := $AudioStreamPlayer2D
+@onready var intro := $Intro
+@onready var main := $Main
+@onready var ending := $Ending
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim_player.play("intro")
 	bgm.play()
-
+	main.visible = false
+	ending.visible = false
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,9 +21,18 @@ func _process(delta):
 
 func intro_finished():
 	anim_player.play("main")
+	intro.visible = false
+	main.visible = true
 
 func main_finished():
 	anim_player.play("ending")
+	main.visible = false
+	ending.visible = true
 	
 func ending_finished():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_skip_pressed():
+	print("hi")
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
