7e8b668d84ba071047ea0b2c5677f2e966e9fb91
WentingLu2022 <wenting.lu@utexas.edu>
2024-03-31 14:38:50 -0500

    challenge_room_update

---
+++ scenes/levels/challenge_room_2.gd
@@ -0,0 +1,34 @@
+extends Level
+
+signal level_complete(next_level:PackedScene)
+
+@onready var bgm := $AudioStreamPlayer2D
+@onready var hell_portal := $HellPortal
+@export var mob_scene: PackedScene
+
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bgm.play()
+	hell_portal.portal_entered.connect(transition)
+	_on_mob_timer_timeout()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func transition():
+	level_complete.emit(next_level)
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	print("drop")
+	
+	var screen_width = get_viewport_rect().size.x
+	var random_x_position = randf_range(0, screen_width)
+	var spawn_height = get_viewport_rect().size.y * -0.1
+	mob.position = Vector2(random_x_position, spawn_height)
+	
+	add_child(mob)
