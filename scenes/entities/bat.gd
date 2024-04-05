2aeede9682853284576047eb2ef3c9b4f18def81
ashnabhansali <91498461+ashnabhansali@users.noreply.github.com>
2024-04-05 17:18:13 -0500

    bat mob created

---
+++ scenes/entities/bat.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+const SPEED = 150
+
+@onready var direction = Vector2.LEFT
+
+@onready var projectile_container = $ProjectileContainer
+@onready var muzzle_1 = $Muzzle1
+@onready var muzzle_2 = $Muzzle2
+@onready var muzzle_3 = $Muzzle3
+@onready var muzzle_4 = $Muzzle4
+@onready var muzzle_5 = $Muzzle5
+var bat_projectile = preload("res://scenes/entities/bat_projectile.tscn")
+
+@onready var animation_tree := $AnimationTree
+@onready var state_machine := $PlayerStateMachine
+
+@export var hit_state : State
+
+# chat gpt helped me with the timer to delay the laser
+@export var timer: float = 1.8
+var interval: float = 2.0
+
+func _ready():
+	animation_tree.active = true
+	
+func _process(delta):
+	timer += delta
+	if timer >= interval:
+		timer = 0.0
+		shoot()
+	
+func _physics_process(delta):
+	pass
+
+func shoot():
+	var projectile_1 = bat_projectile.instantiate()
+	var projectile_2 = bat_projectile.instantiate()
+	var projectile_3 = bat_projectile.instantiate()
+	#var projectile_4 = bat_projectile.instantiate()
+	#var projectile_5 = bat_projectile.instantiate()
+
+	projectile_container.add_child(projectile_1)
+	projectile_1.global_position = muzzle_1.global_position
+	projectile_container.add_child(projectile_2)
+	projectile_2.global_position = muzzle_2.global_position
+	projectile_container.add_child(projectile_3)
+	projectile_3.global_position = muzzle_3.global_position
+	#projectile_container.add_child(projectile_4)
+	#projectile_4.global_position = muzzle_4.global_position
+	#projectile_container.add_child(projectile_5)
+	#projectile_5.global_position = muzzle_5.global_position
---
+++ scenes/entities/bat_projectile.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed = 2.7
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position.y += speed
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_body_entered(body):
+	queue_free()
