f8ec8ddb5acbef88655199f169bb29ff1dc72e9a
kat-wang <kate.a.wang@utexas.edu>
2024-04-10 14:24:54 -0500

    telegraphed that player is saving checkpoint

--- scenes/levels/statue.gd
+++ scenes/levels/statue.gd
@@ -4,16 +4,21 @@ signal save_checkpoint(id)
 
 var in_statue := false
 
+@onready var timer := $Timer
+@onready var label := $Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if in_statue and Input.is_action_just_pressed("interact"):
 		save_checkpoint.emit(get_parent().id)
-
+		timer.start()
+		label.visible = true
+		
+		
 
 func _on_body_entered(body):
 	$Interaction.visible = true
@@ -25,3 +30,7 @@ func _on_body_exited(body):
 	$Interaction.visible = false
 	$Interaction.stop()
 	in_statue = false
+
+
+func _on_timer_timeout():
+	label.visible = false
