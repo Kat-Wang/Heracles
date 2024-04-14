49be4ed44d3103d33a5c53f582cb746aea918482
kat-wang <kate.a.wang@utexas.edu>
2024-04-14 01:33:29 -0500

    added resting level bgms

--- scenes/levels/resting_level.gd
+++ scenes/levels/resting_level.gd
@@ -4,13 +4,16 @@ class_name RestingLevel
 
 signal level_complete(next_level:PackedScene)
 
+@export var id: int
+
 @onready var statue := $Statue
 @onready var resting_portal := $RestingPortal
-@export var id: int
+@onready var bgm := $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	resting_portal.portal_entered.connect(transition)
+	bgm.play()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
