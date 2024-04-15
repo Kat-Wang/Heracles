2294b8834114d90b3ccd6e3ebcbdec7055676680
kat-wang <kate.a.wang@utexas.edu>
2024-04-14 22:23:36 -0500

    added pre-hera cutscene, cleaned up level links

--- scenes/cutscenes/pre_hera.gd
+++ scenes/cutscenes/pre_hera.gd
@@ -1,15 +1,73 @@
 extends Cutscene
 
+class_name PreHera
+
 signal level_complete(next_level:PackedScene)
 
 @onready var anim_player := $AnimationPlayer
 @onready var bgm := $AudioStreamPlayer2D
 @onready var cutscene := $Cutscene
+@onready var happy := $Happy
+@onready var unhappy_broke := $UnhappyBroke
+@onready var unhappy_rich := $UnhappyRich
+@onready var ending := $Ending
+
+var coins : int
 
 func _ready():
-	pass # Replace with function body.
+	anim_player.play("pre_hera")
+	$Cutscene/Cat.play()
+	$Cutscene/BGM.play()
+	happy.visible = false
+	unhappy_broke.visible = false
+	unhappy_rich.visible = false
+	ending.visible = false
+	
+func set_coins(coin_count):
+	coins = coin_count
+	
+func yes():
+	cutscene.visible = false
 
+	if coins < 10:
+		play_unhappy_broke()
+	else:
+		play_happy()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func no():
+		play_unhappy_rich()
+
+func play_happy():
+		happy.visible = true
+		anim_player.play("happy")
+		$Cutscene/BGM.stop()
+		$Happy/BGM.play()
+		$Happy/Cat.play()
+
+func play_unhappy_broke():
+	cutscene.visible = false
+	unhappy_broke.visible = true
+	anim_player.play("unhappy_broke")
+	$Cutscene/BGM.stop()
+	$UnhappyBroke/BGM.play()
+	$UnhappyBroke/Cat.play()
+	
+func play_unhappy_rich():
+	cutscene.visible = false
+	unhappy_rich.visible = true
+	anim_player.play("unhappy_rich")
+	$Cutscene/BGM.stop()
+	$UnhappyRich/BGM.play()
+	$UnhappyRich/Cat.play()
+
+func transition():
+	level_complete.emit(next_level)
+
+func _on_skip_pressed():
+	transition()
+
+func play_ending():
+	#make ending screen scene
+	#put that in and fade into it
+	# for post hera, transition to ending screen through portal
+	ending.visible = true
