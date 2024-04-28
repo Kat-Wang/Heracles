e4ed0d6d181f71666ed9e254aab3ab66ebb71e69
kat-wang <kate.a.wang@utexas.edu>
2024-04-28 13:57:37 -0500

    added ending credits

--- scenes/cutscenes/ending.gd
+++ scenes/cutscenes/ending.gd
@@ -1,13 +1,22 @@
 extends Cutscene
 
 @onready var bgm := $AudioStreamPlayer2D
+@onready var anim_player := $AnimationPlayer
 
 func _ready():
 	$Chicken.play()
 	bgm.play()
+	$CreditsPage.visible = false
 
 func quit():
 	get_tree().quit()
 
 func menu():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func credits():
+	anim_player.play("credits_fadein")
+
+func back():
+	anim_player.play("credits_fadeout")
